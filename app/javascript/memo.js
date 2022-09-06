a62d4edf8b5605c62d4db6f0d6cbfa10a8d10611
ikumi-taka <canomori0311@gmail.com>
2022-09-06 11:54:46 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -4,11 +4,11 @@ function post (){
     e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
-    const XHR = new XMLHttpRequest();  //XMLHttpRequestオブジェクト生成
+    const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
   });
 };
 
-window.addEventListener('load', post); //イベント発火を実行するメソッド 要素.addEventListener('イベント名', 関数)
+window.addEventListener('load', post);
